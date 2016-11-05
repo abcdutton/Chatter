10443f003efbddcc8dbfec4ab302fe6974609810
Alison Dutton <duttonac@gmail.com>
2016-11-05 14:36:59 -0500

    update title

--- src/chatter/handler.clj
+++ src/chatter/handler.clj
@@ -4,19 +4,23 @@
             [ring.middleware.defaults :refer [wrap-defaults site-defaults]]
             [hiccup.page :as page]))
 
+(defn title [n]
+  (str "ClojureBridgeMN" n))
+
 (defn generate-message-view
 "This generates the HTML for displaying messages"
 []
   (page/html5
     [:head
-      [:title "chatter"]]
+      [:title (title 2016)]]
     [:body
       [:h1 "Our Chat App"]]))
+
 (defroutes app-routes
     (GET "/" [] (generate-message-view))
     (route/not-found "Not Found"))
 (comment
-
+(generate-message-view)
 (def x 2)
 (def y 3)
 (* x y 6 5 4 7 6 5 4 7 6 5 4 7)
