1af6c213c993577045bf28b60ba5020cfd02e19e
Alison Dutton <duttonac@gmail.com>
2016-11-05 14:17:12 -0500

    implement hiccup

--- src/chatter/handler.clj
+++ src/chatter/handler.clj
@@ -1,17 +1,26 @@
 (ns chatter.handler
   (:require [compojure.core :refer :all]
             [compojure.route :as route]
-            [ring.middleware.defaults :refer [wrap-defaults site-defaults]]))
+            [ring.middleware.defaults :refer [wrap-defaults site-defaults]]
+            [hiccup.page :as page]))
 
+(defn generate-message-view
+"This generates the HTML for displaying messages"
+[]
+  (page/html5
+    [:head
+      [:title "chatter"]]
+    [:body
+      [:h1 "Our Chat App"]]))
 (defroutes app-routes
-  (GET "/" [] "Good Day World")
-  (route/not-found "Not Found"))
-
+    (GET "/" [] (generate-message-view))
+    (route/not-found "Not Found"))
 (comment
-  
+
 (def x 2)
 (def y 3)
-(* x y 4 5 6 7 ))
+(* x y 6 5 4 7 6 5 4 7 6 5 4 7)
+)
 
 (def app
   (wrap-defaults app-routes site-defaults))
